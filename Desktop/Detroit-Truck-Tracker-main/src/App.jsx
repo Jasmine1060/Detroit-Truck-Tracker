cdc7801dfcdffec584ce1769a4d62912d9e54edc
Jasmine Abu <jasmiabu@umich.edu>
2026-06-25 22:27:24 -0400

    added the district lines

--- Desktop/Detroit-Truck-Tracker-main/src/App.jsx
+++ Desktop/Detroit-Truck-Tracker-main/src/App.jsx
@@ -1,19 +1,43 @@
-import React from 'react';
-import { MapContainer, TileLayer } from 'react-leaflet';
+import React, { useState, useEffect, useRef } from "react";
+import { MapContainer, TileLayer, GeoJSON, LayersControl, LayerGroup, Popup, Marker } from 'react-leaflet';
 
 export default function App() {
+
+  const [geoData, setGeoData] = useState(null);
+  const mapRef = useRef();
+  const position = [42.344863, -83.056870]; 
+
+  useEffect(() => {
+    fetch("/Detroit_City_Council_Districts_2026.geojson")
+    .then((res) => res.json())
+    .then((data) => setGeoData(data))
+    .catch((err) => console.error("Faile to load GeoJSON", err));
+  
+}, []);
+
   return (
-    // 2. Added inline styles to force the map to take up the full screen
-    <MapContainer 
-      center={[42.344863, -83.056870]} 
-      zoom={13} 
-      style={{ height: '100vh', width: '100vw' }}
-    >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-      />
-    </MapContainer>
+    <> 
+      {/* some words */}
+      <h1 style={{ position: 'absolute', top: 10, left: 50, zIndex: 1000, background: 'white', padding: '10px 10px', borderRadius: '0px' }}>
+        Truck Reports
+      </h1>
+    
+
+      {/* starting coordinates/position */}
+      <MapContainer 
+        center={[42.344863, -83.056870]} 
+        zoom={13} 
+        style={{ height: '100vh', width: '100vw' }}
+      >
+        {/* map layer */}
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' /*giving credit*/
+          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' /*the actual map*/
+        />
+        {geoData && <GeoJSON data = {geoData}/> }
+      </MapContainer>
+
+    </> 
   );
 }
 
