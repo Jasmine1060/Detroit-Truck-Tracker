0502d40c6dd71391c49744850bfc33f08f3578a1
Jasmine Abu <jasmiabu@umich.edu>
2026-07-21 15:40:43 -0400

    updated the leaflet for the air page

--- Desktop/Detroit-Truck-Tracker-main/src/Pages/Air.jsx
+++ Desktop/Detroit-Truck-Tracker-main/src/Pages/Air.jsx
@@ -116,8 +116,10 @@ export default function Trucks() {
         style={{ height: '100vh', width: '100vw' }}
       >
         <TileLayer
-          attribution='&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png"
+          attribution= '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+          url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
+          minZoom={0}
+          maxZoom={25}
         />
         <ZoomControl position="bottomright" />
 
